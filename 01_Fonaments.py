22f189c51000b4234d0e00fa451c1b5480242fb9
PereM7 <peremiquel777@gmail.com>
2025-04-10 10:42:38 +0200

    Commit mòdul 1

---
+++ 01_Fonaments.py
@@ -0,0 +1,36 @@
+#Tipus de variables
+nom = "Lucia"      #String
+nombre = 21        #Int
+decimal = 33.3     #Float
+es_boolean = True  #Boolean
+
+#Type mostra es tipus de variable
+print(type(nom))
+print(type(nombre))
+print(type(decimal))
+print(type(es_boolean))
+
+
+#Operadors arimètics
+suma = 5+6
+resta = 5-6
+multip = 5*6
+divisio = 5/6
+modul = 5%6
+
+
+print(f"Suma: {suma}") #Es f{} és per a que pugui imprimir sa variable amb es text
+print(f"Resta: {resta}")
+print(f"Multiplicació: {multip}")
+print(f"Divisió: {divisio}")
+print(f"Mòdul: {modul}")
+
+
+#Operadors per comparar: Igual(==), Diferent(!=), Més que(>), Menys que(<)
+
+
+#Exercici: Calcular l'àrea d'un triangle
+llarg = 10
+ample = 5
+area = llarg*ample
+print(f"L'àrea del triangle és: {area}")
--- hellow.py
+++ hellow.py
@@ -1,5 +1,2 @@
 # Hola món en Python
 print("Hola món en Python")
-
-sum = 5+6
-print(sum)
